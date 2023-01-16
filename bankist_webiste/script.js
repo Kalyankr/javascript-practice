05dd0cab8f41f9b3cdd75a0c42f51929a68a5a2c
kalyankr <kalyan.katla@gmail.com>
2023-01-16 17:22:30 -0600

    tab animations and content implementation

--- bankist_webiste/script.js
+++ bankist_webiste/script.js
@@ -8,17 +8,22 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+// Tabs
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
 const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 };
 
 const closeModal = function (e) {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 };
 
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
@@ -51,4 +56,29 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+// Tabbed component
+tabs.forEach((tab) =>
+  tab.addEventListener("click", function (e) {
+    e.preventDefault();
+  })
+);
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+
+  // active tab
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+  tabsContent.forEach((tabC) =>
+    tabC.classList.remove("operations__content--active")
+  );
+
+  // display content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+  // hide all other default active content
 });
