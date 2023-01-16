d765ba48538c6e305aa14d62a7fffe1b1d055dfa
kalyankr <kalyan.katla@gmail.com>
2023-01-16 14:16:53 -0600

    smooth scrolling with event delegation

--- bankist_webiste/script.js
+++ bankist_webiste/script.js
@@ -8,6 +8,8 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+const section1 = document.querySelector("#section--1");
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -30,8 +32,7 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// implememt smooth scrolling
-const section1 = document.querySelector("#section--1");
+// button smooth scrolling
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
@@ -41,4 +42,13 @@ btnScrollTo.addEventListener("click", function (e) {
     behavior: "smooth",
   });
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// Smooth scrolling with event delegation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
