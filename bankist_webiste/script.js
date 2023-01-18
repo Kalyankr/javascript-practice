fe9f754350e9fb158c59c6ae959c86f345b681b6
kalyankr <kalyan.katla@gmail.com>
2023-01-17 21:35:57 -0600

    lazy image loading

--- bankist_webiste/script.js
+++ bankist_webiste/script.js
@@ -138,3 +138,25 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+// Image selector
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observe) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observe.unoberver(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
