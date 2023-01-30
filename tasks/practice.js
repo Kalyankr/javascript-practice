d45b8592047a1da0867137bbea652f2c3be18c36
kalyankr <kalyan.katla@gmail.com>
2023-01-29 20:08:31 -0600

    async

--- tasks/practice.js
+++ tasks/practice.js
@@ -1,44 +1,44 @@
-// practice promise
+// // practice promise
 
-// //
-// const whereAmI = function (lat, lng) {
-//   return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       console.log(`You are in ${data.city}, ${data.country}`);
+// // //
+// // const whereAmI = function (lat, lng) {
+// //   return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+// //     .then((response) => response.json())
+// //     .then((data) => {
+// //       console.log(data);
+// //       console.log(`You are in ${data.city}, ${data.country}`);
+// //     });
+// // };
+
+// // whereAmI(37.8267, -122.4233);
+// // whereAmI(37.8267, -122.4233);
+// // whereAmI(37.8267, -122.4233);
+
+// //event loop in practice for async code
+// console.log("Test start");
+// setTimeout(() => console.log("0 sec timer"), 0);
+// Promise.resolve("Resolved promise 1").then((res) => console.log(res));
+// Promise.resolve("Resolved promise 2").then((res) => {
+//   for (let i = 0; i < 1000000000; i++) {}
+//   console.log(res);
+// });
+// console.log("Test end");
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement("img");
+//     img.src = imgPath;
+
+//     img.addEventListener("load", function () {
+//       imgContainer.append(img);
+//       resolve(img);
 //     });
+
+//     img.addEventListener("error", function () {
+//       reject(new Error("Image not found"));
+//     });
+//   });
 // };
-
-// whereAmI(37.8267, -122.4233);
-// whereAmI(37.8267, -122.4233);
-// whereAmI(37.8267, -122.4233);
-
-//event loop in practice for async code
-console.log("Test start");
-setTimeout(() => console.log("0 sec timer"), 0);
-Promise.resolve("Resolved promise 1").then((res) => console.log(res));
-Promise.resolve("Resolved promise 2").then((res) => {
-  for (let i = 0; i < 1000000000; i++) {}
-  console.log(res);
-});
-console.log("Test end");
-
-const createImage = function (imgPath) {
-  return new Promise(function (resolve, reject) {
-    const img = document.createElement("img");
-    img.src = imgPath;
-
-    img.addEventListener("load", function () {
-      imgContainer.append(img);
-      resolve(img);
-    });
-
-    img.addEventListener("error", function () {
-      reject(new Error("Image not found"));
-    });
-  });
-};
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -46,19 +46,74 @@ const getPosition = function () {
   });
 };
 
-const whereAmI = async function () {
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+// const whereAmI = async function () {
+//   const pos = await getPosition();
+//   const { latitude: lat, longitude: lng } = pos.coords;
 
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const dataGeo = await resGeo.json();
+//   const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//   const dataGeo = await resGeo.json();
 
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
-  );
-  const data = await res.json();
-  renderCountry(data[0]);
-  return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+//   const res = await fetch(
+//     `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+//   );
+//   const data = await res.json();
+//   renderCountry(data[0]);
+//   return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+// };
+// whereAmI();
+// console.log("FIRST");
+
+renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 };
-whereAmI();
-console.log("FIRST");
+
+const whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error("Problem getting location data");
+    const dataGeo = await resGeo.json();
+
+    // Country data
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+    );
+    if (!resGeo.ok) throw new Error("Problem getting country");
+    const data = await res.json();
+    renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
+  }
+};
+
+console.log("1: Will get location");
+
+whereAmI()
+  .then((city) => console.log(`2: ${city}`))
+  .catch((err) => console.error(`2: ${err.message} 💥`))
+  .finally(() => console.log("3: Finished getting location"));
