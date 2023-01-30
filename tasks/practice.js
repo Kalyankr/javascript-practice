b1e50f3afc4aeb3aa498fa9395afb1331217c7ef
kalyankr <kalyan.katla@gmail.com>
2023-01-30 12:23:46 -0600

    Async fetch practice

--- tasks/practice.js
+++ tasks/practice.js
@@ -40,11 +40,11 @@
 //   });
 // };
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
 
 // const whereAmI = async function () {
 //   const pos = await getPosition();
@@ -63,57 +63,107 @@ const getPosition = function () {
 // whereAmI();
 // console.log("FIRST");
 
-renderCountry = function (data, className = "") {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
+// renderCountry = function (data, className = "") {
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const whereAmI = async function () {
-  try {
-    // Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
+// const whereAmI = async function () {
+//   try {
+//     // Geolocation
+//     const pos = await getPosition();
+//     const { latitude: lat, longitude: lng } = pos.coords;
 
-    // Reverse geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error("Problem getting location data");
-    const dataGeo = await resGeo.json();
+//     // Reverse geocoding
+//     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//     if (!resGeo.ok) throw new Error("Problem getting location data");
+//     const dataGeo = await resGeo.json();
 
-    // Country data
-    const res = await fetch(
-      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
-    );
-    if (!resGeo.ok) throw new Error("Problem getting country");
-    const data = await res.json();
-    renderCountry(data[0]);
+//     // Country data
+//     const res = await fetch(
+//       `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+//     );
+//     if (!resGeo.ok) throw new Error("Problem getting country");
+//     const data = await res.json();
+//     renderCountry(data[0]);
 
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
-  } catch (err) {
-    console.error(`${err} 💥`);
-    renderError(`💥 ${err.message}`);
+//     return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+//   } catch (err) {
+//     console.error(`${err} 💥`);
+//     renderError(`💥 ${err.message}`);
 
-    // Reject promise returned from async function
-    throw err;
-  }
-};
+//     // Reject promise returned from async function
+//     throw err;
+//   }
+// };
 
-console.log("1: Will get location");
+// console.log("1: Will get location");
 
-whereAmI()
-  .then((city) => console.log(`2: ${city}`))
-  .catch((err) => console.error(`2: ${err.message} 💥`))
-  .finally(() => console.log("3: Finished getting location"));
+// whereAmI()
+//   .then((city) => console.log(`2: ${city}`))
+//   .catch((err) => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log("3: Finished getting location"));
+
+// const loadscript = function (src, callback) {
+//   let script = document.createElement("script");
+//   script.src = src;
+//   script.onload = () => callback(script);
+//   document.head.append(script);
+// };
+
+// loadscript(
+//   "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js",
+//   (script) => {
+//     console.log(script);
+//     console.log(_);
+//   }
+// );
+
+// let promise = new Promise(function (resolve, reject) {
+//   // the function is executed automatically when the promise is constructed
+
+//   // after 1 second signal that the job is done with the result "done"
+//   setTimeout(() => resolve("done"), 1000);
+// });
+
+// let promiseE = new Promise(function (resolve, reject) {
+//   // after 1 second signal that the job is finished with an error
+//   setTimeout(() => reject(new Error("Whoops!")), 1000);
+// });
+
+// function delay(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+// delay(3000).then(() => alert("runs after 3 seconds"));
+
+// Make a request for user.json
+fetch("/article/promise-chaining/user.json")
+  // Load it as json
+  .then((response) => response.json())
+  // Make a request to GitHub
+  .then((user) => fetch(`https://api.github.com/users/${user.name}`))
+  // Load the response as json
+  .then((response) => response.json())
+  // Show the avatar image (githubUser.avatar_url) for 3 seconds (maybe animate it)
+  .then((githubUser) => {
+    let img = document.createElement("img");
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+
+    setTimeout(() => img.remove(), 3000); // (*)
+  });
