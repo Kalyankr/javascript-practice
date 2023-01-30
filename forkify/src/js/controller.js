f3af7b6e7bb62571cb85a051386a74a6f47a9481
kalyankr <kalyan.katla@gmail.com>
2023-01-30 17:52:40 -0600

    forkify recipe app init and fetach implementation basic

---
+++ forkify/src/js/controller.js
@@ -0,0 +1,30 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/get?rId=47746'
+    );
+    if (!res.ok) throw new Error(`Problem with recipe ${res.status}`);
+
+    const data = await res.json();
+    console.log(data.recipe.publisher);
+    console.log(data.recipe.ingredients);
+    console.log(data.recipe.source_url);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
